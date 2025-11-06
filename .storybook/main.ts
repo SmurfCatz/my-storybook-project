d713304af99ee4b18bd146cbe5d82476d94b2138
SmurfCatz <jek2online@gmail.com>
2025-11-06 16:24:39 +0700

    build: configure Vite, TailwindCSS, and Storybook build setup

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+
+export default config;
