0348c6d1bdee46bcaa176e70525a8ca373f9f291
SmurfCatz <jek2online@gmail.com>
2025-11-06 16:33:50 +0700

    test: add Storybook story for Footer component

---
+++ src/stories/Footer.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Footer from '../components/Footer';
+import { expect, within } from 'storybook/test';
+
+const meta = {
+  title: 'Components/Footer',
+  component: Footer,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    
+    // ทดสอบว่าหัวข้อ "© 2025 Storybook. All rights reserved." ถูก render แล้ว
+    expect(canvas.getByText('© 2025 Storybook. All rights reserved.')).toBeInTheDocument();
+  },
+};
