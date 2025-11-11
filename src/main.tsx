2b713e22f2179be4e3e75477251fb14e98f1b668
SmurfCatz <jek2online@gmail.com>
2025-11-11 11:54:41 +0700

    feat: add BrowserRouter form react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import BlogDetail from "./components/BlogDetail.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
