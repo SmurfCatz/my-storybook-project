f2deb8d880511cbdee0363f5069a8bee471665c6
SmurfCatz <jek2online@gmail.com>
2025-11-11 16:22:17 +0700

    refactor: use react route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 // src/App.tsx
-import { ReactNode } from 'react';
-import TabBar from './components/TabBar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import { Pages, useTabState } from './hooks/useTabState';
+import { ReactNode } from "react";
+import TabBar from "./components/TabBar";
+import Footer from "./components/Footer";
+import { Pages, useTabState } from "./hooks/useTabState";
+import { Outlet, useLocation } from "react-router-dom";
 
 interface AppProps {
   children?: ReactNode;
@@ -14,36 +12,28 @@ interface AppProps {
 }
 
 function App({ children, currentPage: propPage, changePage: propChange }: AppProps) {
-  // ใช้ props ถ้ามี (สำหรับ Storybook), มิฉะนั้นใช้ hook (สำหรับแอปจริง)
-  const { currentPage: hookPage, changePage: hookChange } = 
-    propPage === undefined ? useTabState() : { currentPage: propPage, changePage: propChange! };
+  const { currentPage: hookPage, changePage: hookChange } =
+    propPage === undefined
+      ? useTabState()
+      : { currentPage: propPage!, changePage: propChange! };
 
   const currentPage = propPage ?? hookPage;
   const changePage = propChange ?? hookChange;
-
-  const renderContent = () => {
-    switch (currentPage) {
-      case Pages.HOME:
-        return <Home />;
-      case Pages.ABOUT:
-        return <About />;
-      case Pages.CONTACT:
-        return <Contact />;
-      default:
-        return <Home />;
-    }
-  };
+  const location = useLocation();
+  const isBlogDetail = location.pathname.startsWith("/blog/");
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <div className="flex-1 flex flex-col">
+        {/* TabBar) */}
         <header className="bg-white shadow-sm border-b">
           <TabBar currentPage={currentPage} onChangePage={changePage} />
         </header>
 
-        <main className="flex-1 p-6">
+        {/* Main Content */}
+        <main className="flex-1">
           <div className="bg-white rounded-xl shadow-sm p-8 min-h-full">
-            {renderContent()}
+            <Outlet />
             {children}
           </div>
         </main>
--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import BlogDetail from "./components/BlogDetail.tsx";
 import "./index.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/blog/:id" element={<BlogDetail />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="blog/:id" element={<BlogDetail />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
